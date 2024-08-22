e1c06a9577c420fb883b9567aa01c0eba3761985
lokesh168 <lokesh.vervali@gmail.com>
2024-08-22 14:49:01 +0530

    add vite integration

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,7 @@
 const hamburgerMenu = document.querySelector(".icon-hamburger");
 const navigationMenu = document.querySelector(".primary-navigation");
-
 const mobileMenu = document.getElementById("primary-navigation-menu");
 
 hamburgerMenu.addEventListener("click", () => {
-    console.log("Clicked classes");
-    // navigationMenu.classList.toggle("primary-navigation");
-
     mobileMenu.classList.toggle("visibility-hidden");
 });
